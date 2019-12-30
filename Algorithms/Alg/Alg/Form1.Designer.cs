4a5f84099c2ec62f85ecdfefc93d3b9b0045cfcf
howtus <howtus12@gmail.com>
2019-12-30 03:44:38 +0300

    Добавил ZedGraph

---
+++ Algorithms/Alg/Alg/Form1.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace Alg
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.zedGraph = new ZedGraph.ZedGraphControl();
+            this.SuspendLayout();
+            // 
+            // zedGraph
+            // 
+            this.zedGraph.Location = new System.Drawing.Point(13, 13);
+            this.zedGraph.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.zedGraph.Name = "zedGraph";
+            this.zedGraph.ScrollGrace = 0D;
+            this.zedGraph.ScrollMaxX = 0D;
+            this.zedGraph.ScrollMaxY = 0D;
+            this.zedGraph.ScrollMaxY2 = 0D;
+            this.zedGraph.ScrollMinX = 0D;
+            this.zedGraph.ScrollMinY = 0D;
+            this.zedGraph.ScrollMinY2 = 0D;
+            this.zedGraph.Size = new System.Drawing.Size(774, 436);
+            this.zedGraph.TabIndex = 0;
+            this.zedGraph.UseExtendedPrintDialog = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.zedGraph);
+            this.Name = "Form1";
+            this.Text = "График";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ZedGraph.ZedGraphControl zedGraph;
+    }
+}
+
---
+++ Algorithms/Alg/Alg/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ZedGraph;
+
+namespace Alg
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            PointPairList list = new PointPairList();
+            GraphPane pane = zedGraph.GraphPane;
+
+            pane.XAxis.Title.Text = "Ось X";
+            pane.YAxis.Title.Text = "Ось Y";
+            pane.Title.Text = "Бабочка";
+
+            //for (double x = -7; x <7; x+=0.001)
+            //{
+            //    double y = 2 * Math.Sqrt((-Math.Abs(Math.Abs(x) - 1)) * Math.Abs(3 - Math.Abs(
+            //    x)) / ((Math.Abs(x) - 1) * (3 - Math.Abs(x)))) * (1 + Math.Abs(Math.Abs(x) - 3) / (Math.Abs(x) - 3)) *
+            //    Math.Sqrt(1 - Math.Pow((x / 7), 2)) + (5 + 0.97 * (Math.Abs(x - 0.5) + Math.Abs(x + 0.5)) - 3 * (Math.Abs(
+            //    x - 0.75) + Math.Abs(x + 0.75))) *
+            //    (1 + Math.Abs(1 - Math.Abs(x)) / (1 - Math.Abs(x)));
+            //    list.Add(x, y);
+            //    
+            //}
+            //for(double x = -7; x < 7; x += 0.001)
+            //{
+            //    double y = (-3) * Math.Sqrt(1 - Math.Pow((x / 7), 2)) * Math.Sqrt(Math.Abs(Math.Abs(x) - 4) / (Math.Abs(x) - 4));
+            //    list.Add(x, y);
+            //}
+            //
+            //for (double x = -7; x < 7; x += 0.001)
+            //{
+            //    double y = Math.Abs(x / 2)-Math.Pow(0.0913722 * x,2) - 3 + Math.Sqrt(1 - Math.Pow((Math.Abs(Math.Abs(x) - 2) - 1),2));
+            //    list.Add(x, y);
+            //}
+            //for (double x = -7; x < 7; x += 0.001)
+            //{
+            //    double y = (2.71052 +1.5 - 0.5 * Math.Abs(x) - 1.35526 * Math.Sqrt(4 - Math.Pow((Math.Abs(x) - 1) , 2))) * Math.Sqrt(Math.Abs(
+            //    Math.Abs(x) - 1) / (Math.Abs(x) - 1)) + 0.9;
+            //    list.Add(x, y);
+            //}
+
+            for (double t = 0; t <= 12 * Math.PI; t+=0.001)
+            {
+                double x = Math.Sin(t) * (Math.Pow(Math.E, Math.Cos(t)) - 2 * Math.Cos(4 * t) + Math.Pow(Math.Sin(t / 12), 5));
+                double y = Math.Cos(t) * (Math.Pow(Math.E, Math.Cos(t)) - 2 * Math.Cos(4 * t) + Math.Pow(Math.Sin(t / 12), 5));
+                list.Add(x, y);
+            }
+
+            LineItem myCurve = pane.AddCurve("", list, Color.Red, SymbolType.None);
+            zedGraph.AxisChange();
+            zedGraph.Invalidate();
+        }
+
+    }
+}
+
